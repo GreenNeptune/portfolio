fe0cd80068efafe819e238b4fe5671d470cae167
GreenNeptune <silentwisdom75@gmail.com>
2020-06-02 17:16:27 +0300

    Updates

---
+++ precache-manifest.6599ac691ef26543aba767f142853ce9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f1980e58b7a0ddbfe319db58dfe9dc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10cc8dadc12e46710b7b",
+    "url": "/portfolio/static/css/2.e8e2b19f.chunk.css"
+  },
+  {
+    "revision": "5a8f64a456d48f14ddbd",
+    "url": "/portfolio/static/css/main.3482a6a1.chunk.css"
+  },
+  {
+    "revision": "10cc8dadc12e46710b7b",
+    "url": "/portfolio/static/js/2.037ab130.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.037ab130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8f64a456d48f14ddbd",
+    "url": "/portfolio/static/js/main.22913129.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "a42271f782edb0b1cd48722be39d2e38",
+    "url": "/portfolio/static/media/Bali_Product.a42271f7.PNG"
+  },
+  {
+    "revision": "08241da46ed850f2254a7336fc5faa0d",
+    "url": "/portfolio/static/media/TV_Shows_search.08241da4.PNG"
+  },
+  {
+    "revision": "ef22aa5e343b8c90d6fb9600f5fae969",
+    "url": "/portfolio/static/media/chuck_norris_jokes.ef22aa5e.PNG"
+  },
+  {
+    "revision": "6fc11e40497f2e119f622a26e2034858",
+    "url": "/portfolio/static/media/minecraft_project_logo.6fc11e40.PNG"
+  }
+]);
